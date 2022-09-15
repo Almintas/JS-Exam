29c6a36cf3e57005fefbc04f42a107e5e9e44d72
Almintas <almintasbruzas@gmail.com>
2022-09-15 18:59:45 +0300

    Task nr 4 done

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,35 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const response = fetch(ENDPOINT);
+const output = document.getElementById('output');
+
+response
+.then((res) => res.json())
+.then((data) => {
+    const ulElement = document.createElement('ul');
+
+    data.forEach((userData) => {
+        const liElement = document.createElement('li');
+        liElement.textContent = `${userData.brand}`;
+        ulElement.append(liElement);
+        liElement.style.textAlign = 'center';
+        liElement.style.fontSize = 'large';
+        liElement.style.padding = '15px';
+        liElement.style.border = 'solid';
+        liElement.style.borderBottom = 'none';
+
+        const newElement = document.createElement('p');
+        newElement.textContent = `${userData.models}`;
+        ulElement.append(newElement);
+        newElement.style.textAlign = 'center';
+        newElement.style.border = 'solid';
+        newElement.style.borderTop = 'none';
+        newElement.style.padding = '20px';
+    });
+    
+    output.append(ulElement);
+    document.body.style.maxWidth = '500px';
+    document.body.style.margin = 'auto';
+});
